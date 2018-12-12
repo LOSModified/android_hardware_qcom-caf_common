187b513c2352016f6af28a9c9f5ccd9db0578d17
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-12 20:02:09 +0530

    BoardConfigQcom: Allow a device to manually override which HALs it wants to use
    Like for an example: although ZUK Z2 is actually msm8996, its community built 4.4 kernel
    needs msm8998 HALs for proper functionality (VIDC doesn't work at all with msm8996 media HAL).
    This is done by adding the following line in BoardConfig:
    OVERRIDE_QCOM_HARDWARE_VARIANT := msm8998
    
    Change-Id: Icf26be96facad5638abd5fb269c41f4e852c16a9

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -341,6 +341,11 @@ endif
 # Pass board platform to kernel build
 TARGET_KERNEL_ADDITIONAL_FLAGS += TARGET_BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 
+# Allow a device to manually override which HALs it wants to use
+ifneq ($(OVERRIDE_QCOM_HARDWARE_VARIANT),)
+QCOM_HARDWARE_VARIANT := $(OVERRIDE_QCOM_HARDWARE_VARIANT)
+endif
+
 # Allow a device to opt-out hardset of PRODUCT_SOONG_NAMESPACES
 QCOM_SOONG_NAMESPACE ?= hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
 PRODUCT_SOONG_NAMESPACES += $(QCOM_SOONG_NAMESPACE)
